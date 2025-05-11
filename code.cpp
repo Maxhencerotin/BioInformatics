6799278f8b8f0c10bad7a8f5d0032319bac2837d
Maxhencerotin <max.henrotin@gmail.com>
2025-05-11 14:23:23 +0200

    working to find the principal consensus with spoa

--- code.cpp
+++ code.cpp
@@ -6,8 +6,6 @@
 #include <string>
 #include <cctype>
 #include <cstdlib>
-#include <limits>
-#include <algorithm>
 using namespace std;
 using namespace spoa;  
 
@@ -59,38 +57,47 @@ void print_sequence_list(const vector<string>& sequences) {
 //__________________________________METHODS FOR THE ALGORITHM____________________________
 //k-medoid algorithm --> using SPOA
 
-// Fonction pour trouver les k medoids
-vector<string> find_k_medoids(const vector<string>& seq, int k) {
-    std::vector<std::string> sequences = {
-        "CATAAAAGAACGTAGGTCGCCCGTCCGTAACCTGTCGGATCACCGGAAAGGACCCGTAAAGTGATAATGAT",
-        "ATAAAGGCAGTCGCTCTGTAAGCTGTCGATTCACCGGAAAGATGGCGTTACCACGTAAAGTGATAATGATTAT",
-        "ATCAAAGAACGTGTAGCCTGTCCGTAATCTAGCGCATTTCACACGAGACCCGCGTAATGGG",
-        "CGTAAATAGGTAATGATTATCATTACATATCACAACTAGGGCCGTATTAATCATGATATCATCA",
-        "GTCGCTAGAGGCATCGTGAGTCGCTTCCGTACCGCAAGGATGACGAGTCACTTAAAGTGATAAT",
-        "CCGTAACCTTCATCGGATCACCGGAAAGGACCCGTAAATAGACCTGATTATCATCTACAT"
-    };
+// function that align all the sequences from the argument
+vector<string> generate_msa(const vector<string>& sequences) {
   
     auto alignment_engine = spoa::AlignmentEngine::Create(
-        spoa::AlignmentType::kNW, 3, -5, -3);  // linear gaps
+        spoa::AlignmentType::kNW, 3, -5, -3);  // linear gaps   (3 : Score de correspondance entre les bases. / -5 : Pénalité pour une ouverture de gap. / -3 : Pénalité pour une extension de gap.)
   
-    spoa::Graph graph{};
+    spoa::Graph graph{};    //to store alignment sequences
   
+    //iterate over all sequences and put them in the graph at the right place regarding the alignment_engine
     for (const auto& it : sequences) {
       auto alignment = alignment_engine->Align(it, graph);
       graph.AddAlignment(alignment, it);
     }
   
-    auto consensus = graph.GenerateConsensus();
+    //generate a consensus
+    string consensus = graph.GenerateConsensus();
   
-    std::cerr << ">Consensus LN:i:" << consensus.size() << std::endl
-              << consensus << std::endl;
+    //generate the multiple sequence alignment
+    vector<string> msa = graph.GenerateMultipleSequenceAlignment();
+
+    return msa;
+}
+
+//find one consensus string (maybe can be mixed together with the msa method)
+string find_consensus(const vector<string>& sequences) {
+
+    auto alignment_engine = spoa::AlignmentEngine::Create(
+        spoa::AlignmentType::kNW, 3, -5, -3);  // linear gaps   (3 : Score de correspondance entre les bases. / -5 : Pénalité pour une ouverture de gap. / -3 : Pénalité pour une extension de gap.)
   
-    auto msa = graph.GenerateMultipleSequenceAlignment();
+    spoa::Graph graph{};    //to store alignment sequences
   
-    for (const auto& it : msa) {
-      std::cerr << it << std::endl;
+    //iterate over all sequences and put them in the graph at the right place regarding the alignment_engine
+    for (const auto& it : sequences) {
+      auto alignment = alignment_engine->Align(it, graph);
+      graph.AddAlignment(alignment, it);
     }
-    return seq;
+  
+    //generate a consensus
+    string consensus = graph.GenerateConsensus();
+
+    return consensus;
 }
 
 //_________________________EXECUTION__________________________________
@@ -107,12 +114,32 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    //extract sequences
     string file_path = argv[1];
     vector<string> sequences = extract_sequences(file_path);
     vector<string> sequences296 = keep_seq_length(sequences, 296, 296);
-    print_sequence_list(sequences296);
-    cout << sequences.size() << endl;
-    cout << sequences296.size() << endl;
+    vector<string> sequences250_350 = keep_seq_length(sequences, 250, 350);
+    cout << sequences.size() << "sequences in total" << endl;
+    cout << sequences296.size() << "sequences of size 296" << endl;
+    cout << sequences250_350.size() << "sequences of a size from 250 to 350" << endl;
+
+
+    //alignment
+    vector<string> msa = generate_msa(sequences296);
+    //print_sequence_list(msa);
+
+
+    //consensus
+    cout << endl << "_______CONSENSUS_OVER_SIZE_296_SEQ__________" << endl; //for J29B correct consensus 
+    string consensus296 = find_consensus(sequences296);
+    cout << consensus296 << endl << endl;
+    cout << endl << "_______CONSENSUS_OVER_ALL_SEQ__________" << endl;  //for J29B there is 2 errors in the principal consensus
+    string consensus = find_consensus(sequences);
+    cout << consensus << endl << endl;
+    cout << endl << "_______CONSENSUS_OVER_ALL_SEQ_FROM_250_TO_350__________" << endl;  //for J29B there is no errors in the principal consensus
+    string consensus250_350 = find_consensus(sequences250_350);
+    cout << consensus250_350 << endl << endl;
+
 
     return 0;
 }

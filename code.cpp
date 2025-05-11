96a1bbb3363580d7ad80a200af797fea0f582923
Maxhencerotin <max.henrotin@gmail.com>
2025-05-11 13:55:32 +0200

    spoa library installed

--- code.cpp
+++ code.cpp
@@ -1,11 +1,15 @@
 //code of the poject of Lynn Limbach & Max Henrotin
+#include <spoa/spoa.hpp>    //use chatgpt to see how to install spoa library on your computer (use linux)
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <cctype>
 #include <cstdlib>
+#include <limits>
+#include <algorithm>
 using namespace std;
+using namespace spoa;  
 
 //____________________HELPERS FOR SEQUENCES EXTRACTION___________________________
 //this method extract in an array all the ACTG sequences from the .fastq file
@@ -54,9 +58,49 @@ void print_sequence_list(const vector<string>& sequences) {
 
 //__________________________________METHODS FOR THE ALGORITHM____________________________
 //k-medoid algorithm --> using SPOA
-//more testing
+
+// Fonction pour trouver les k medoids
+vector<string> find_k_medoids(const vector<string>& seq, int k) {
+    std::vector<std::string> sequences = {
+        "CATAAAAGAACGTAGGTCGCCCGTCCGTAACCTGTCGGATCACCGGAAAGGACCCGTAAAGTGATAATGAT",
+        "ATAAAGGCAGTCGCTCTGTAAGCTGTCGATTCACCGGAAAGATGGCGTTACCACGTAAAGTGATAATGATTAT",
+        "ATCAAAGAACGTGTAGCCTGTCCGTAATCTAGCGCATTTCACACGAGACCCGCGTAATGGG",
+        "CGTAAATAGGTAATGATTATCATTACATATCACAACTAGGGCCGTATTAATCATGATATCATCA",
+        "GTCGCTAGAGGCATCGTGAGTCGCTTCCGTACCGCAAGGATGACGAGTCACTTAAAGTGATAAT",
+        "CCGTAACCTTCATCGGATCACCGGAAAGGACCCGTAAATAGACCTGATTATCATCTACAT"
+    };
+  
+    auto alignment_engine = spoa::AlignmentEngine::Create(
+        spoa::AlignmentType::kNW, 3, -5, -3);  // linear gaps
+  
+    spoa::Graph graph{};
+  
+    for (const auto& it : sequences) {
+      auto alignment = alignment_engine->Align(it, graph);
+      graph.AddAlignment(alignment, it);
+    }
+  
+    auto consensus = graph.GenerateConsensus();
+  
+    std::cerr << ">Consensus LN:i:" << consensus.size() << std::endl
+              << consensus << std::endl;
+  
+    auto msa = graph.GenerateMultipleSequenceAlignment();
+  
+    for (const auto& it : msa) {
+      std::cerr << it << std::endl;
+    }
+    return seq;
+}
 
 //_________________________EXECUTION__________________________________
+
+//to compile : g++ -o code code.cpp -lspoa
+//OR ?? : g++ -o code code.cpp -I~/spoa/spoa/include -L~/spoa/build/lib -lspoa
+//to execute : ./code fastq/J29_B_CE_IonXpress_005.fastq    //example of execution for this file
+
+
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {    //fastq/J29_B_CE_IonXpress_005.fastq  is one example of argument
         cerr << "Please provide the path of the FASTQ file." << endl;
